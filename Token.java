85bff01dfb17711ed510306388568b66ab7eed82
abhishek792 <abhishek.chauhan792@gmail.com>
2014-07-08 17:31:26 +0530

    Added a data member and completed setter

--- Token.java
+++ Token.java
@@ -1,23 +1,28 @@
 public class Token {
-	private int isfree;
+	private boolean isfree;
 	private int iscomplete;
 	private int position;
-	private int playerid;
-	public Token(int id, int playerId) {
-		coordinate = playerId * 13 - 1;
-		state = GAME_CONFIG.LOCKED;
-		this.id = id;
+	private int playerId;
+	private int stepsMoved;
+	public Token(int playerId) {
+		
 		this.playerId = playerId;
+		this.position = playerId * 12;
+		this.isfree = false ;
+		this.iscomplete = false;
+		this.stepsMoved = 0;
+		
 	}
 	public int getPlayerId(){
 		return playerId;
 	}
 	public boolean canMove(int steps) {
+		return (isfree && (stepsMoved + steps) > 60 &&  )
 		return (coordinate - playerId * 13 + steps <= 60  && state != GAME_CONFIG.LOCKED) || (state == GAME_CONFIG.LOCKED && steps == 6);
 	}
 	public void reset(){
-		coordinate = playerId * 13 - 1;
-		state = GAME_CONFIG.LOCKED;
+		coordinate = playerId * 12;
+		isfree = false;
 	}
 	public boolean moveToken(int steps) {
 		if (!canMove(steps))
