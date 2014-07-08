45a347a0d7abf45ba38c1756c4703c5145848a6f
abhishek792 <abhishek.chauhan792@gmail.com>
2014-07-08 17:42:08 +0530

    methods implemented

--- Token.java
+++ Token.java
@@ -3,50 +3,41 @@ public class Token {
 	private int iscomplete;
 	private int position;
 	private int playerId;
-	private int stepsMoved;
 	public Token(int playerId) {
 		
 		this.playerId = playerId;
 		this.position = playerId * 12;
 		this.isfree = false ;
 		this.iscomplete = false;
-		this.stepsMoved = 0;
 		
 	}
 	public int getPlayerId(){
 		return playerId;
 	}
-	public boolean canMove(int steps) {
-		return (isfree && (stepsMoved + steps) > 60 &&  )
-		return (coordinate - playerId * 13 + steps <= 60  && state != GAME_CONFIG.LOCKED) || (state == GAME_CONFIG.LOCKED && steps == 6);
-	}
+	
 	public void reset(){
+		
 		coordinate = playerId * 12;
 		isfree = false;
+		
 	}
-	public boolean moveToken(int steps) {
-		if (!canMove(steps))
-			return false;
-		coordinate = coordinate + steps;
-		if (coordinate - playerId*13 == 60) {
-			state = GAME_CONFIG.FINISHED;
-		}else if(coordinate - playerId*13 >=55) {
-			state = GAME_CONFIG.SAFE;
-		}
-		return true;
+	public void moveToken(int steps) {
+		
+		position += steps;	
+		
 	}
 	
-	public void setState(int state) {
-		this.state = state;
+	public void free() {
+		this.isfree = true;
 	}
-	public int getId() {
-		return this.id;
+	public int getplayerId() {
+		return this.playerid;
 	}
-	public int getCoordinate() {
-		return coordinate;
+	public int getPosition() {
+		return position;
 	}
-	public int getState() {
-		return state;
+	public int isfree() {
+		return isfree;
 	}
 	
 }
