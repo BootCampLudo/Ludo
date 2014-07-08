8fbd0c729a5d9a99fef9bae0a59dafaf81d4060b
sharathbcs10 <sharath_bcs10@nitc.ac.in>
2014-07-08 14:59:46 +0530

    Created Token.java

---
+++ Token.java
@@ -0,0 +1,47 @@
+public class Token {
+	private int state;
+	private int coordinate;
+	private int id;
+	private int playerId;
+	public Token(int id, int playerId) {
+		coordinate = playerId * 13 - 1;
+		state = GAME_CONFIG.LOCKED;
+		this.id = id;
+		this.playerId = playerId;
+	}
+	public int getPlayerId(){
+		return playerId;
+	}
+	public boolean canMove(int steps) {
+		return (coordinate - playerId * 13 + steps <= 60  && state != GAME_CONFIG.LOCKED) || (state == GAME_CONFIG.LOCKED && steps == 6);
+	}
+	public void reset(){
+		coordinate = playerId * 13 - 1;
+		state = GAME_CONFIG.LOCKED;
+	}
+	public boolean moveToken(int steps) {
+		if (!canMove(steps))
+			return false;
+		coordinate = coordinate + steps;
+		if (coordinate - playerId*13 == 60) {
+			state = GAME_CONFIG.FINISHED;
+		}else if(coordinate - playerId*13 >=55) {
+			state = GAME_CONFIG.SAFE;
+		}
+		return true;
+	}
+	
+	public void setState(int state) {
+		this.state = state;
+	}
+	public int getId() {
+		return this.id;
+	}
+	public int getCoordinate() {
+		return coordinate;
+	}
+	public int getState() {
+		return state;
+	}
+	
+}
